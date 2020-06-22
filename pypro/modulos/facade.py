4bd6873ed29841fc7101117a78e57c967358fc94
ivansilva86 <63621288+ivansilva86@users.noreply.github.com>
2020-06-22 18:14:19 -0300

    Criação de App de Módulos de Aulas

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -3,10 +3,14 @@ from typing import List
 from pypro.modulos.models import Modulo
 
 
-def listar_modulos_ordenados()-> List[Modulo]:
+def listar_modulos_ordenados() -> List[Modulo]:
     """
     Lista Módulos ordenados por títulos
     :return:
     """
 
     return list(Modulo.objects.order_by('order').all())
+
+
+def encontrar_modulo(slug: str) ->Modulo:
+    return Modulo.objects.get(slug=slug)
